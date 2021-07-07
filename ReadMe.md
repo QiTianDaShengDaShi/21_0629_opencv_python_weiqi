26e8762ec513a0604942ce982174d56ce5097c3c
diyun_git <2532098972@qq.com>
2021-07-07 16:33:50 +0800

    [ADD]提交工程代码

---
+++ ReadMe.md
@@ -0,0 +1,21 @@
+# 使用说明:
+
+# 前提准备
+需要安装opencv库
+
+# 1、test1：单张图片检测
+直接运行21_0629_opencv_python_weiqi_test1.py即可。
+## 效果图
+![在这里插入图片描述](https://img-blog.csdnimg.cn/20210707150000205.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L21hb19odWlfZmVp,size_16,color_FFFFFF,t_70)
+![在这里插入图片描述](https://img-blog.csdnimg.cn/20210707150133857.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L21hb19odWlfZmVp,size_16,color_FFFFFF,t_70)
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/20210707150024613.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L21hb19odWlfZmVp,size_16,color_FFFFFF,t_70)
+![在这里插入图片描述](https://img-blog.csdnimg.cn/20210707150042921.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L21hb19odWlfZmVp,size_16,color_FFFFFF,t_70)
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/20210707150103413.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L21hb19odWlfZmVp,size_16,color_FFFFFF,t_70)
+
+# 2、test2：多张图片检测，输出顺序
+运行 21_0629_opencv_python_weiqi_test2.py
+前提是screen文件夹里的原始图像是按照顺序从1、2、3...排列的。
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/20210707150155184.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L21hb19odWlfZmVp,size_16,color_FFFFFF,t_70)
---
+++ app/21_0629_opencv_python_weiqi_test2.py
@@ -0,0 +1,266 @@
+from PIL import ImageGrab
+import numpy as np
+import cv2
+from glob import glob
+import os
+import time
+
+
+#Python将数字转换成大写字母
+def getChar(number):
+    factor, moder = divmod(number, 26) # 26 字母个数
+    modChar = chr(moder + 65)          # 65 -> 'A'
+    if factor != 0:
+        modChar = getChar(factor-1) + modChar # factor - 1 : 商为有效值时起始数为 1 而余数是 0
+    return modChar
+def getChars(length):
+    return [getChar(index) for index in range(length)]
+
+
+
+"""  "*******************************************************************************************
+*函数功能 ：统计二值化图片黑色像素点百分比
+*输入参数 ：输入裁剪后图像，
+*返 回 值 ：返回黑色像素点占比0-1之间
+*编写时间 ： 2021.6.30
+*作    者 ： diyun
+********************************************************************************************"""
+def Heise_zhanbi(img):
+    [height, width, tongdao] = img.shape
+    #print(width, height, tongdao)
+    # cv2.imshow("3", img)
+    # cv2.waitKey(20)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # cv2.imshow("binary", gray)
+    # cv2.waitKey(100)
+
+    # etVal, threshold = cv2.threshold(gray, 125, 255, cv2.THRESH_BINARY)
+    # # cv2.imshow("threshold", threshold)
+    # # cv2.waitKey(200)
+    # a = 0
+    # b = 0
+    # for row in range(height):
+    #     for col in range(width):
+    #         val = threshold[row][col]
+    #         if (val) == 0:#黑色
+    #             a = a + 1
+    #         else:
+    #             b = b + 1
+
+    a = np.sum(gray < 125)
+    zhanbi = (float)(a) / (float)(height*width)
+    #print("黑色像素个数", a, "黑色像素占比", zhanbi)
+    return zhanbi
+
+
+"""  "*******************************************************************************************
+*函数功能 ：统计二值化图片白色像素点百分比
+*输入参数 ：输入裁剪后图像，
+*返 回 值 ：返回白色像素点占比0-1之间
+*编写时间 ： 2021.6.30
+*作    者 ： diyun
+********************************************************************************************"""
+def Baise_zhanbi(img):
+    [height, width, tongdao] = img.shape
+    #print(width, height, tongdao)
+    # cv2.imshow("3", img)
+    # cv2.waitKey(20)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # cv2.imshow("binary", gray)
+    # cv2.waitKey(100)
+
+    # etVal, threshold = cv2.threshold(gray, 235, 255, cv2.THRESH_BINARY)
+    # # cv2.imshow("threshold", threshold)
+    # # cv2.waitKey(200)
+    # a = 0
+    # b = 0
+    # for row in range(height):
+    #     for col in range(width):
+    #         val = threshold[row][col]
+    #         if (val) == 0:#黑色
+    #             a = a + 1
+    #         else:
+    #             b = b + 1
+    b=np.sum(gray>235)
+    zhanbi = (float)(b) / (float)(height*width)
+    #print("白色像素个数", b, "白色像素占比", zhanbi)
+    return zhanbi
+
+"""  "*******************************************************************************************
+*函数功能 ：定位棋盘位置
+*输入参数 ：截图
+*返 回 值 ：裁剪后的图像
+*编写时间 ： 2021.6.30
+*作    者 ： diyun
+********************************************************************************************"""
+def dingweiqizi_weizhi(img):
+    '''********************************************
+    1、定位棋盘位置
+    ********************************************'''
+    #img = cv2.imread("./screen/1.jpg")
+
+    image = img.copy()
+    w, h, c = img.shape
+    img2 = np.zeros((w, h, c), np.uint8)
+    img3 = np.zeros((w, h, c), np.uint8)
+    # img = ImageGrab.grab() #bbox specifies specific region (bbox= x,y,width,height *starts top-left)
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower = np.array([10, 0, 0])
+    upper = np.array([40, 255, 255])
+    mask = cv2.inRange(hsv, lower, upper)
+    erodeim = cv2.erode(mask, None, iterations=2)  # 腐蚀
+    dilateim = cv2.dilate(erodeim, None, iterations=2)
+
+    img = cv2.bitwise_and(img, img, mask=dilateim)
+    frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, dst = cv2.threshold(frame, 100, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(dst, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+
+
+    #cv2.imshow("0", img)
+
+    i = 0
+    maxarea = 0
+    nextarea = 0
+    maxint = 0
+    for c in contours:
+        if cv2.contourArea(c) > maxarea:
+            maxarea = cv2.contourArea(c)
+            maxint = i
+        i += 1
+
+    # 多边形拟合
+    epsilon = 0.02 * cv2.arcLength(contours[maxint], True)
+    if epsilon < 1:
+        print("error :   epsilon < 1")
+        pass
+
+    # 多边形拟合
+    approx = cv2.approxPolyDP(contours[maxint], epsilon, True)
+    [[x1, y1]] = approx[0]
+    [[x2, y2]] = approx[2]
+
+    checkerboard = image[y1:y2, x1:x2]
+    # cv2.imshow("1", checkerboard)
+    # cv2.waitKey(1000)
+    #cv2.destroyAllWindows()
+    return checkerboard
+
+"""  "*******************************************************************************************
+*函数功能 ：定位棋子颜色及位置
+*输入参数 ：裁剪后的图像
+*返 回 值 ：棋子颜色及位置列表
+*编写时间 ： 2021.6.30
+*作    者 ： diyun
+********************************************************************************************"""
+def dingweiqizi_yanse_weizhi(img):
+    '''********************************************
+    2、识别棋盘棋子位置及颜色及序号；
+    ********************************************'''
+    #img = cv2.imread("./checkerboard/checkerboard_1.jpg")
+    img = cv2.resize(img, (724,724), interpolation=cv2.INTER_AREA)
+    #cv2.imshow("src",img)
+    #cv2.waitKey(1000)
+
+    #变量定义
+    small_length=38  #每个小格宽高
+    qizi_zhijing=38#棋子直径
+    zuoshangjiao=20#棋盘四周的宽度
+
+    list = [[0 for i in range(19)] for j in range(19)]
+    #print(list)
+
+    for i in range(19):
+        for j in range(19):
+
+            lie = i
+            hang = j
+
+            Tp_x = small_length * lie
+            Tp_y = small_length * hang
+            Tp_width = qizi_zhijing
+            Tp_height = qizi_zhijing
+
+            img_temp=img[Tp_y:Tp_y+Tp_height, Tp_x:Tp_x+Tp_width]#参数含义分别是：y、y+h、x、x+w
+
+            heise_zhanbi=Heise_zhanbi(img_temp)
+            if heise_zhanbi>0.5:
+                list[hang][lie]=2#黑色
+                #print("当前棋子为黑色")
+            else:
+                baise_zhanbi = Baise_zhanbi(img_temp)
+                if baise_zhanbi > 0.15:
+                    list[hang][lie] = 1  # 黑色
+                    #print("当前棋子为白色")
+                else:
+                    list[hang][lie] = 0  # 黑色
+                    #print("当前位置没有棋子")
+            #print(heise_zhanbi)
+    #cv2.imshow("2",img)
+    #print("\n")
+    #print(list)
+    return  list
+
+"""  "*******************************************************************************************
+*函数功能 ：标准化输出
+*输入参数 ：棋子位置及颜色上次和这次列表、上次标准输出，调用次数
+*返 回 值 ：标准输出，例如：
+*编写时间 ： 2021.6.30
+*作    者 ： diyun
+********************************************************************************************"""
+def biaozhunhuashuchu(list0,list1,list2,connter):
+    a_array = np.array(list0)
+    b_array = np.array(list1)
+    d_array = b_array-a_array
+
+    for i in range(19):
+        for j in range(19):
+            if d_array[i][j]>1:
+                #pass
+                print("第",i,"行，第",j,"列棋子为黑色：",i,j)
+                A = getChars(i + 1)
+                B = getChars(j + 1)
+                str0 = str(connter) + str(A[i]) + str(B[j])
+                #print(str0)
+                list2.append(str0)
+            elif d_array[i][j]>0:
+                #pass
+                print("第", i, "行，第", j, "列棋子为白色：", i, j)
+                A = getChars(i+1)
+                B = getChars(j+1)
+                str0 = str(connter) + str(A[i]) + str(B[j])
+                #print(str0)
+                list2.append(str0)
+
+    return list2
+
+if __name__ =="__main__":
+    list0 = [[0 for i in range(19)] for j in range(19)]
+    list_finall = []
+    imglist = sorted(glob("screen/*.jpg"))
+    connter = 0
+    for i in imglist:
+        #img = cv2.imread("./screen/8.jpg")
+        img = cv2.imread(i)
+        '''********************************************
+        1、定位棋盘位置
+        ********************************************'''
+        img_after=dingweiqizi_weizhi(img)
+        #cv2.imshow("src",img)
+
+        '''********************************************
+        2、识别棋盘棋子位置及颜色及序号；
+        ********************************************'''
+        list1=dingweiqizi_yanse_weizhi(img_after)
+
+        '''********************************************
+        3、标准化输出
+        ********************************************'''
+        connter=connter+1
+        list_finall=biaozhunhuashuchu(list0,list1,list_finall,connter)
+        list0=list1
+        print(list_finall)
+
+
